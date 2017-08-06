aa14061e668a5f374ff14f09a65b60acec326f1f
Patrick Solis <psolis00@gmail.com>
2017-08-05 18:19:10 -0700

    added smooth scrolling

--- home.js
+++ home.js
@@ -9,3 +9,30 @@ function openNav(event){
 function closeNav(event){  
     document.getElementById("mySidebar").style.width = "0px";
 }
+
+$(document).ready(function(){
+
+    $("#aboutButton").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+
+    $("#homeButton").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 1000);
+    });
+
+    $("#projectsButton").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 1000);
+    });
+
+    $("#contactButton").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+});
