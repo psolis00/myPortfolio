6159b0541aaebd6c802dd319012d1c1ad4f4ef89
Patrick Solis <psolis00@gmail.com>
2017-08-07 22:31:36 -0700

    Added submenu with direct link

--- home.js
+++ home.js
@@ -10,17 +10,58 @@ function closeNav(event){
     document.getElementById("mySidebar").style.width = "0px";
 }
 
+function openAboutSub(){
+    document.getElementById("aSub").style.height = "116px";
+    document.getElementById("projectsButton").style.marginTop = "116px";
+}
+
+function openProjectsSub(){
+    document.getElementById("pSub").style.height = "75px";
+    document.getElementById("contactButton").style.marginTop = "75px";
+}
+
+function closeAboutSub(){
+    document.getElementById("aSub").style.height = "0px";
+    document.getElementById("projectsButton").style.marginTop = "0px";
+}
+
+function closeProjectsSub(){
+    document.getElementById("pSub").style.height = "0px";
+    document.getElementById("contactButton").style.marginTop = "0px";
+}
+
 $(document).ready(function(){
+    
+    $("#homeButton").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 1000);
+        closeAboutSub();
+        closeProjectsSub();
+    });
 
     $("#aboutButton").click(function(){
         $('html, body').animate({
             scrollTop: $("#about").offset().top
         }, 1000);
+        closeProjectsSub();
     });
 
-    $("#homeButton").click(function(){
+    $("#bio").click(function(){
         $('html, body').animate({
-            scrollTop: $("#home").offset().top
+            scrollTop: $("#miag").offset().top
+        }, 1000);
+    });
+
+    $("#skills").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#ss").offset().top
+        }, 1000);
+    });
+
+    $("#edu").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#education").offset().top
         }, 1000);
     });
 
@@ -28,11 +69,26 @@ $(document).ready(function(){
         $('html, body').animate({
             scrollTop: $("#projects").offset().top
         }, 1000);
+        closeAboutSub();
+    });
+
+    $("#p1").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#etes").offset().top
+        }, 1000);
+    });
+
+    $("#p2").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#brca1").offset().top
+        }, 1000);
     });
 
     $("#contactButton").click(function(){
         $('html, body').animate({
             scrollTop: $("#contact").offset().top
         }, 1000);
+        closeAboutSub();
+        closeProjectsSub();
     });
 });
